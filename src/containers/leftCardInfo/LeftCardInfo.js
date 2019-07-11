03e8d928d4366eeee2d176cdfd6fcacda54f0e4c
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-07-11 17:54:34 -0600

    cardInfo more responsive

--- src/containers/leftCardInfo/LeftCardInfo.js
+++ src/containers/leftCardInfo/LeftCardInfo.js
@@ -18,6 +18,20 @@ export class LeftCardInfo extends Component {
             <article style={{height: "100%"}}>
               <div className="pokemon-name">
                 <h2>{pokemon.name}</h2>
+                <div className="pokemon-hp-mobile">
+                {
+                  pokemon.hp ? (
+                    <h2>{`HP ${pokemon.hp}`}</h2>
+                  ) : null
+                }
+                {
+                  pokemon.types ? (
+                    pokemon.types.map((type, i) => {
+                    return(<img key={`type-${i}`}src={require(`../../assets/${type}.png`)} alt={type} />)
+                  })
+                  ) : null
+                }
+              </div>
               </div>
               <div className="image-container">
                 <img src={pokemon.imageUrlHiRes}  alt="card"/>
